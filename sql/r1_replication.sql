7f8ff5b185a2542850ad938f1940bd52e5eacbb2
NikolayS <github2@samokhvalov.com>
2019-05-22 13:08:42 -0400

    [WIP] New report: r1 - replication

---
+++ sql/r1_replication.sql
@@ -0,0 +1,20 @@
+/* In Postgres 9.6 and earlier use:
+\set postgres_dba_wal_lsn_diff pg_xlog_location_diff
+\set postgres_dba_wal_current_lsn pg_current_xlog_location
+\set postgres_dba_col_sent_lsn sent_location
+\set postgres_dba_col_write_lsn write_location
+\set postgres_dba_col_flush_lsn flush_location
+\set postgres_dba_col_replay_lsn replay_location
+*/
+
+
+select
+  client_addr, usename, application_name, state, sync_state,
+  pg_size_pretty(:postgres_dba_wal_lsn_diff(:postgres_dba_wal_current_lsn(), :postgres_dba_col_sent_lsn)) as pending_lag,
+  pg_size_pretty(:postgres_dba_wal_lsn_diff(:postgres_dba_col_sent_lsn, :postgres_dba_col_write_lsn)) as write_lag,
+  pg_size_pretty(:postgres_dba_wal_lsn_diff(:postgres_dba_col_write_lsn, :postgres_dba_col_flush_lsn)) as flush_lag,
+  pg_size_pretty(:postgres_dba_wal_lsn_diff(:postgres_dba_col_flush_lsn, :postgres_dba_col_replay_lsn)) as replay_lag,
+  pg_size_pretty(:postgres_dba_wal_lsn_diff(:postgres_dba_wal_current_lsn(), coalesce(:postgres_dba_col_replay_lsn, :postgres_dba_col_flush_lsn))) as total_lag,
+  slots.*
+from pg_stat_replication
+left join pg_replication_slots slots on pid = active_pid;
